dd1ca800aa62c6bf00b0e9bc09c33680d17816b4
ydvprbn <prabin.ydv098@gmail.com>
2025-03-01 11:02:10 +0545

    created student model

--- studywithme/student/models.py
+++ studywithme/student/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    grade = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} (Grade: {self.grade})"
